5ffe55e3aeddc407e1e5c267a9ecc088d03a2453
Kate Smychok <e.smychok@yandex.ru>
2021-07-20 17:21:23 +0300

    feat: create project components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
