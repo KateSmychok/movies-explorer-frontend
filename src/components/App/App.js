684ee4c4c1486f482a08f4edcd3c4cce88f39874
Kate Smychok <e.smychok@yandex.ru>
2021-07-22 10:30:38 +0300

    feat: add basic routes in App

---
+++ src/components/App/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+
+function App() {
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header />
+        <Switch>
+          <Route path='/'>
+            <MainPage />
+          </Route>
+          <Route path='/movies'>
+            <MoviesPage />
+          </Route>
+          <Route path='/saved-movies'>
+            <SavedMoviesPage />
+          </Route>
+          <Route path='/profile'>
+            <ProfilePage />
+          </Route>
+          <Route path='/signup'>
+            <Register />
+          </Route>
+          <Route path='/signin'>
+            <Login />
+          </Route>
+        </Switch>
+        <Footer/>
+      </div>
+    </CurrentUserContext.Provider>
+  )
+}
+
+export default App;
