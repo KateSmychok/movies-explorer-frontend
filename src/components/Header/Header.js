fdac5ce9d4b5182ce3e307edbe46a49b969b0cce
Kate Smychok <e.smychok@yandex.ru>
2021-07-30 13:41:27 +0300

    feat: add Header styles for MoviesPage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import cn from 'classnames';
-import HeaderStyles from './Header.module.scss';
+import styles from './Header.module.scss';
 
 function Header() {
   return (
     <Switch>
-      <Route path='/'>
-        <div className={cn(HeaderStyles.header, HeaderStyles.headerMain)}>
-          <Link to="/" className={HeaderStyles.logo}></Link>
-          <div className={HeaderStyles.buttonsArea}>
-            <Link to="/signup" className={HeaderStyles.regButton}>Регистрация</Link>
-            <Link to="/signin" className={HeaderStyles.authButton}>Войти</Link>
+      <Route exact path='/'>
+        <div className={cn(styles.header, styles.headerLanding)}>
+          <Link to="/" className={styles.logo}></Link>
+          <div className={styles.buttonsArea}>
+            <Link to="/signup" className={styles.regButton}>Регистрация</Link>
+            <Link to="/signin" className={styles.authButton}>Войти</Link>
           </div>
+        </div>
+      </Route>
+      <Route path='/movies'>
+        <div className={cn(styles.header, styles.headerMain)}>
+          <nav className={styles.leftSide}>
+            <Link to="/" className={cn(styles.logo, styles.logoMain)}></Link>
+            <Link to="/movies" className={styles.navItem}>Фильмы</Link>
+            <Link to="/saved-movies" className={styles.navItem}>Сохранённые фильмы</Link>
+          </nav>
+          <button className={styles.profileButton}>Аккаунт</button>
         </div>
       </Route>
     </Switch>
