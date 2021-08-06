684d0a348643c4cbc9e9ce2f9bc9851b0c0d44a8
Kate Smychok <e.smychok@yandex.ru>
2021-08-06 18:31:50 +0300

    refactor: make FilterCheckbox separate component

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import styles from './SearchForm.module.scss';
-import cn from 'classnames/bind';
-
-let cx = cn.bind(styles);
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 function SearchForm() {
-  const [isTumblerOn, setIsTumblerOn] = React.useState(true);
-
-  let tumblerClassName = cx({
-    baseTumbler: true,
-    tumblerOn: isTumblerOn,
-    tumblerOff: !isTumblerOn,
-  });
-
-  const handleTumblerClick = () => {
-    setIsTumblerOn(!isTumblerOn);
-  }
 
   return (
     <section className={styles.searchSection}>
       <div className={styles.wholeForm}>
         <form className={styles.searchForm}>
           <input className={styles.searchInput}/>
-          <button className={styles.searchButton}></button>
+          <button className={styles.searchButton}> </button>
         </form>
-        <div className={styles.shortFilmsTumbler}>
-          <div className={styles.border}></div>
-          <button
-            className={tumblerClassName}
-            onClick={handleTumblerClick}>
-          </button>
-          <p className={styles.caption}>Короткометражки</p>
-        </div>
+        <div className={styles.border}> </div>
+        <FilterCheckbox />
       </div>
     </section>
   )
