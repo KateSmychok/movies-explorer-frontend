dd7f52b706c9280f36bbc1689e9c76e8a3aee7f0
Kate Smychok <e.smychok@yandex.ru>
2021-08-06 23:35:51 +0300

    feat: add EditProfilePopup jsx

---
+++ src/components/EditProfilePopup/EditProfilePopup.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from './EditProfilePopup.module.scss';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import cn from 'classnames/bind';
+
+let cx = cn.bind(styles);
+
+function EditProfilePopup(props) {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const user = React.useContext(CurrentUserContext);
+
+  React.useEffect(() => {
+    setName(user.name);
+    setEmail(user.email);
+  }, [user]);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  let popupClassName = cx({
+    popup: true,
+    popupOpened: props.isPopupOpened,
+  });
+
+  return (
+    <div className={popupClassName}>
+      <div className={styles.popupContent}>
+        <h3 className={styles.title}>Редактировать профиль</h3>
+        <form name='profile' className={styles.form}>
+          <input
+            className={styles.input}
+            value={name || ''}
+            onChange={handleNameChange}
+            id='name'
+            autoComplete='off'
+            type='text'
+            name='name'
+            placeholder='Имя'
+            minLength='2'
+            maxLength='40'
+            required />
+          <span className={styles.inputError} id='name-error'> </span>
+          <input
+            className={styles.input}
+            value={email || ''}
+            onChange={handleEmailChange}
+            id='email'
+            autoComplete='off'
+            type='text'
+            name='email'
+            placeholder='Email'
+            minLength='6'
+            maxLength='40'
+            required />
+          <span className={styles.inputError} id='email-error'> </span>
+          <button type='submit' className={styles.submitButton}>Сохранить</button>
+        </form>
+        <button type='button' className={styles.closeButton} onClick={props.onClose}> </button>
+      </div>
+    </div>
+  );
+}
+
+export default EditProfilePopup;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import styles from './Profile.module.scss';
 import cn from 'classnames';
 import {Link} from "react-router-dom";
 import CurrentUserContext from '../../contexts/CurrentUserContext';
+import EditProfilePopup from '../EditProfilePopup/EditProfilePopup';
 
 function Profile() {
   const user = React.useContext(CurrentUserContext);
@@ -12,24 +13,31 @@ function Profile() {
     setIsPopupOpened(true);
   }
 
+  const handleCloseButtonClick = () => {
+    setIsPopupOpened(false);
+  }
+
   return (
-    <section className={styles.profileSection}>
-      <h2 className={styles.title}>
-        Привет, {user.name}!
-      </h2>
-      <div className={styles.userInfo}>
-        <div className={styles.userInfoLine}>
-          <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>Имя</p>
-          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.name}</p>
+    <>
+      <section className={styles.profileSection}>
+        <h2 className={styles.title}>
+          Привет, {user.name}!
+        </h2>
+        <div className={styles.userInfo}>
+          <div className={styles.userInfoLine}>
+            <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>Имя</p>
+            <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.name}</p>
+          </div>
+          <div className={styles.userInfoLine}>
+            <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>E-mail</p>
+            <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.email}</p>
+          </div>
         </div>
-        <div className={styles.userInfoLine}>
-          <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>E-mail</p>
-          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.email}</p>
-        </div>
-      </div>
-      <button type='button' className={cn(styles.button, styles.buttonEdit)} onClick={handleEditButtonClick}>Редактировать</button>
-      <Link to='/signin' className={cn(styles.button, styles.buttonSignOut)}>Выйти из аккаунта</Link>
-    </section>
+        <button type='button' className={cn(styles.button, styles.buttonEdit)} onClick={handleEditButtonClick}>Редактировать</button>
+        <Link to='/signin' className={cn(styles.button, styles.buttonSignOut)}>Выйти из аккаунта</Link>
+      </section>
+      <EditProfilePopup isPopupOpened={isPopupOpened} onClose={handleCloseButtonClick} />
+    </>
   )
 }
 
