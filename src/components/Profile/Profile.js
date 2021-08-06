6da1ae1e77d214d694ed42a01079331359070340
Kate Smychok <e.smychok@yandex.ru>
2021-08-06 18:29:56 +0300

    feat: use user Context in Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import styles from './Profile.module.scss';
 import cn from 'classnames';
+import {Link} from "react-router-dom";
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 
 function Profile() {
+  const user = React.useContext(CurrentUserContext);
+  const [isPopupOpened, setIsPopupOpened] = React.useState(false);
+
+  const handleEditButtonClick = () => {
+    setIsPopupOpened(true);
+  }
+
   return (
     <section className={styles.profileSection}>
       <h2 className={styles.title}>
-        Привет, Катя!
+        Привет, {user.name}!
       </h2>
       <div className={styles.userInfo}>
         <div className={styles.userInfoLine}>
           <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>Имя</p>
-          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>Катя</p>
+          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.name}</p>
         </div>
         <div className={styles.userInfoLine}>
           <p className={cn(styles.userInfoText, styles.userInfoTextKey)}>E-mail</p>
-          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>kate@yandex.ru</p>
+          <p className={cn(styles.userInfoText, styles.userInfoTextValue)}>{user.email}</p>
         </div>
       </div>
-      <button className={cn(styles.button, styles.buttonEdit)}>Редактировать</button>
-      <button className={cn(styles.button, styles.buttonSignOut)}>Выйти из аккаунта</button>
+      <button type='button' className={cn(styles.button, styles.buttonEdit)} onClick={handleEditButtonClick}>Редактировать</button>
+      <Link to='/signin' className={cn(styles.button, styles.buttonSignOut)}>Выйти из аккаунта</Link>
     </section>
   )
 }
