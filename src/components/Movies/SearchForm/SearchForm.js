027c5e18f3639cfa8c563144a94ba20d577ab742
Kate Smychok <e.smychok@yandex.ru>
2021-08-10 14:52:56 +0300

    feat: make search form validation

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,20 +1,61 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import styles from './SearchForm.module.scss';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
+const SearchSchema = Yup.object().shape({
+  keyword: Yup.string()
+    .min(2, 'Минимальная длина - 2 символа')
+    .max(40, 'Максимальная длина запроса - 40 символов')
+    .required('Нужно ввести ключевое слово для поиска'),
+});
+
 function SearchForm() {
   return (
-    <section className={styles.searchSection}>
-      <div className={styles.wholeForm}>
-        <form className={styles.searchForm}>
-          <input className={styles.searchInputDesktop}/>
-          <input className={styles.searchInputMobile} placeholder='Фильм'/>
-          <button className={styles.searchButton}> </button>
-        </form>
-        <div className={styles.border}> </div>
-        <FilterCheckbox />
-      </div>
-    </section>
+    <div>
+      <Formik
+        initialValues={{
+          keyword: '',
+        }}
+
+        validationSchema={SearchSchema}
+
+        onSubmit={ (values) => {
+          console.log(values);
+        }}
+      >
+        {({
+          errors,
+          touched,
+          dirty,
+          isValid,
+        }) => (
+          <section className={styles.searchSection}>
+            <div className={styles.formWithTumbler}>
+              <Form className={styles.form}>
+                <Field
+                  name='keyword'
+                  type='text'
+                  className={styles.input}
+                  id='keyword'
+                  placeholder='Фильм'
+                />
+                {errors.keyword && touched.keyword ? (
+                  <span className={styles.inputError}>
+                    {errors.keyword}</span>
+                ) : <span className={styles.inputError}> </span>
+                }
+
+                <button type='submit' className={styles.submitButton} disabled={!(dirty && isValid)}> </button>
+              </Form>
+              <div className={styles.border}> </div>
+              <FilterCheckbox />
+            </div>
+          </section>
+        )}
+      </Formik>
+    </div>
   );
 }
 
