1775994507dda1f6ead21edd2732ef768aaaf899
Kate Smychok <e.smychok@yandex.ru>
2021-08-11 22:04:44 +0300

    feat: add InfoToolTip component

---
+++ src/components/InfoToolTip/InfoTooltip.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import cn from 'classnames/bind';
+import styles from './InfoTooltip.module.scss';
+
+const cx = cn.bind(styles);
+
+function InfoToolTip(props) {
+  function successOrFail() {
+    let text;
+    if (props.isSuccess) {
+      text = 'Вы успешно зарегистрировались!';
+    } else {
+      text = 'Что-то пошло не так! Попробуйте ещё раз.';
+    }
+    return text;
+  }
+
+  return (
+    <div className={cx({
+      overlay: true,
+      opened: props.isOpened,
+    })}>
+      <div className={styles.content}>
+        <div className={cx({
+          image: true,
+          imageSuccess: props.isSuccess,
+          imageFail: !props.isSuccess,
+        })}> </div>
+        <h3 className={styles.caption}>{successOrFail()}</h3>
+        <button type='button' className={styles.closeButton} onClick={props.onClose}> </button>
+      </div>
+    </div>
+  );
+}
+
+export default InfoToolTip;
