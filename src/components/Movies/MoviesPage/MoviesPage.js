59644de2148319b6db30747def1034f87e532ef3
Kate Smychok <e.smychok@yandex.ru>
2021-08-28 20:37:55 +0300

    feat: short movies filter checkbox

--- src/components/Movies/MoviesPage/MoviesPage.js
+++ src/components/Movies/MoviesPage/MoviesPage.js
@@ -11,7 +11,7 @@ function MoviesPage(props) {
   const [allMovies, setAllMovies] = React.useState([]);
   const [filteredMovies, setFilteredMovies] = React.useState([]);
   const [moviesToRender, setMoviesToRender] = React.useState([]);
-  const [shortMovies, setShortMovies] = React.useState([]);
+  const [longMovies, setLongMovies] = React.useState([]);
 
   const [btnLoadMoreIsVisible, setBtnLoadMoreIsVisible] = React.useState(false);
   const [preloaderIsVisible, setPreloaderIsVisible] = React.useState(false);
@@ -44,6 +44,7 @@ function MoviesPage(props) {
   // Сабмит формы поиска
   const handleSearchBtnSubmit = ({ keyword }) => {
     localStorage.removeItem('movies');
+    localStorage.removeItem('long-movies');
     setDefaultStates();
     setPreloaderIsVisible(true);
     // Фильтр по ключевому слову
@@ -56,18 +57,18 @@ function MoviesPage(props) {
       localStorage.setItem('movies', JSON.stringify(filMovies));
     // Если чекбокс -
     } else if (!checked && filMovies.length > 0) {
-      const longMovies = filMovies.filter(
+      const movies = filMovies.filter(
         (item) => item.duration > 40,
       );
-      if (longMovies.length > 0) {
-        setFilteredMovies(longMovies);
-        localStorage.setItem('movies', JSON.stringify(longMovies));
+      if (movies.length > 0) {
+        setLongMovies(movies);
+        localStorage.setItem('long-movies', JSON.stringify(movies));
       }
       setPreloaderIsVisible(false);
       // Если ничего не найдено
     } else {
-      console.log('test1');
       setFilteredMovies([]);
+      setLongMovies([]);
       setDefaultStates();
       setPreloaderIsVisible(false);
       setMessageIsVisible(true);
@@ -98,45 +99,53 @@ function MoviesPage(props) {
 
   // Ререндер фильмов при изменении ключевого слова или количества карточек для рендера
   React.useEffect(() => {
-    console.log('test');
-    if (filteredMovies.length > 0) {
-      getMoviesToRender(filteredMovies);
-    } else if (localStorage.getItem('movies') && filteredMovies.length === 0) {
-      const moviesInLocalStorage = JSON.parse(localStorage.getItem('movies'));
-      getMoviesToRender(moviesInLocalStorage);
+    if (checked) {
+      if (filteredMovies.length > 0) {
+        getMoviesToRender(filteredMovies);
+      } else if (localStorage.getItem('movies') && filteredMovies.length === 0) {
+        const moviesInLocalStorage = JSON.parse(localStorage.getItem('movies'));
+        getMoviesToRender(moviesInLocalStorage);
+      } else {
+        setTimeout(() => {
+          setDefaultStates();
+        }, 500);
+      }
     } else {
-      setTimeout(() => {
-        setDefaultStates();
-        setPreloaderIsVisible(false);
-      }, 500);
+      if (longMovies.length > 0) {
+        getMoviesToRender(longMovies);
+      } else if (localStorage.getItem('long-movies') && longMovies.length === 0) {
+        const moviesInLocalStorage = JSON.parse(localStorage.getItem('long-movies'));
+        getMoviesToRender(moviesInLocalStorage);
+      } else {
+        setTimeout(() => {
+          setDefaultStates();
+        }, 500);
+      }
     }
-  }, [filteredMovies, maxCards]);
+  }, [filteredMovies, maxCards, longMovies]);
 
-  // Получение отфильтрованных по длительности фильмов для рендера
+  // Получение фильмов > 40 мин.
   const getFilteredByDurationMovies = (films) => {
     if (!checked && films.length > 0) {
       const movies = films.filter(
         (item) => item.duration > 40,
       );
-      const shortM = films.filter(
-        (item) => item.duration <= 40,
-      );
-      setShortMovies(shortM);
       if (movies.length > 0) {
-        setFilteredMovies(movies);
-        localStorage.setItem('movies', JSON.stringify(movies));
+        setLongMovies(movies);
+        localStorage.setItem('long-movies', JSON.stringify(movies));
       } else {
-        localStorage.removeItem('movies');
         setDefaultStates();
         setMessageIsVisible(true);
         props.setErrMessage('Ничего не найдено');
       }
+    } else if (checked) {
+      setLongMovies([]);
+      localStorage.removeItem('long-movies');
     }
   };
 
   // Ререндер короткометражек при изменении стейта чекбокса
   React.useEffect(() => {
-    console.log('test 3');
     if (filteredMovies.length > 0) {
       getFilteredByDurationMovies(filteredMovies);
     } else if (localStorage.getItem('movies') && filteredMovies.length === 0) {
